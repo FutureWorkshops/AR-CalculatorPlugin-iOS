3fba5aa4cd3c3a3d32b9448bf2e363979c422bb4
Matt Brooke-Smith <matt@futureworkshops.com>
2022-11-23 18:29:08 +0300

    Update SumCalculatorStep
    * Add parsing from app.json

--- CalculatorPlugin/CalculatorPlugin/SumCalculationStep.swift
+++ CalculatorPlugin/CalculatorPlugin/SumCalculationStep.swift
@@ -8,23 +8,24 @@ import Foundation
 import MobileWorkflowCore
 import SwiftUI
 
-public struct SumCalculationItem: Codable, Identifiable {
+public struct SumCalculationItem: Codable, Identifiable, Equatable {
     public let id: String
-    public let label: String
+    public let text: String
     public var value: Int = 0
 }
  
 
 public class SumCalculationStep: ObservableStep {
-    let type: String?
+    let type: String
     let items: [SumCalculationItem]
     let allowUserToAddItems: Bool?
 
-    public init(identifier: String, type: String?, items: [SumCalculationItem], allowUserToAddItems: Bool?, session: Session, services: StepServices) {
+    public init(identifier: String, type: String, text: String?, items: [SumCalculationItem], allowUserToAddItems: Bool?, session: Session, services: StepServices) {
         self.type = type
         self.items = items
         self.allowUserToAddItems = allowUserToAddItems
         super.init(identifier: identifier, session: session, services: services)
+        self.text = text
     }
 
     public override func instantiateViewController() -> StepViewController {
@@ -39,12 +40,34 @@ extension SumCalculationStep: BuildableStep {
     }
 
     public static func build(stepInfo: StepInfo, services: StepServices) throws -> Step {
-        let type = stepInfo.data.content["type"] as? String
-        guard let items = stepInfo.data.content["items"] as? Any else {
+        guard let type = stepInfo.data.content["type"] as? String else {
+            throw ParseError.invalidStepData(cause: "Mandatory type property not found")
+        }
+        
+        let text = stepInfo.data.content["text"] as? String
+        
+        guard let items = stepInfo.data.content["items"] as? [[String: Any]] else {
             throw ParseError.invalidStepData(cause: "Mandatory items property not found")
         }
+        
+        let calculatorItems: [SumCalculationItem] = try items.map {
+            return try makeSumCalculatorItem(with: $0)
+        }
+        
         let allowUserToAddItems = stepInfo.data.content["allowUserToAddItems"] as? Bool
-        return SumCalculationStep(identifier: stepInfo.data.identifier, type: type, items: [], allowUserToAddItems: allowUserToAddItems, session: stepInfo.session, services: services)
+        return SumCalculationStep(identifier: stepInfo.data.identifier, type: type, text: text, items: calculatorItems, allowUserToAddItems: allowUserToAddItems, session: stepInfo.session, services: services)
+    }
+    
+    private static func makeSumCalculatorItem(with item: [String: Any]) throws -> SumCalculationItem {
+        guard let id = item.getString(key: "id") else {
+            throw ParseError.invalidStepData(cause: "Invalid id for step")
+        }
+        
+        guard let text = item["text"] as? String else {
+            throw ParseError.invalidStepData(cause: "Invalid text for step")
+        }
+        
+        return SumCalculationItem(id: id, text: text)
     }
 }
 
@@ -66,30 +89,30 @@ struct SumCalculationStepContentView: View {
     var navigator: Navigator { step.navigator }
     @State var items: [SumCalculationItem]
     @State private var presentAlert = false
-    @State private var nextItemLabel = ""
+    @State private var nextItemText = ""
     private var total: Int {
         items.reduce(0, { $0 + $1.value })
     }
 
     var body: some View {
         Form {
-            Section {
+            Section(header: Text(step.text ?? "")) {
                 ForEach($items) { $item in
                     SumCalculationItemView(item: $item)
                 }
                 Button("Add Item") {
-                    nextItemLabel = ""
+                    nextItemText = ""
                     presentAlert = true
                 }
-                .alert("Login", isPresented: $presentAlert, actions: {
-                    TextField("Name", text: $nextItemLabel)
-                    
+                .alert("Add Item", isPresented: $presentAlert, actions: {
+                    TextField("Name", text: $nextItemText)
+
                     Button("Cancel", role: .cancel, action: {})
                     Button("Add", action: {
-                        items.append(SumCalculationItem(id: UUID().uuidString, label: nextItemLabel))
+                        items.append(SumCalculationItem(id: UUID().uuidString, text: nextItemText))
                     })
                 }, message: {
-                    Text("Add a new item.")
+                    Text("Add a new item to the calculation.")
                 })
             }
             Section {
@@ -110,20 +133,26 @@ struct SumCalculationStepContentView: View {
 
 struct SumCalculationItemView: View {
     private let formatter: NumberFormatter = NumberFormatter()
-    @State private var text: String = ""
+    @State private var val: String = ""
     @Binding var item: SumCalculationItem
     
     var body: some View {
         HStack {
-            Text(item.label)
+            Text(item.text)
                 .font(.headline)
-            TextField("Tap to enter", text: $text)
+            TextField("Tap to enter", text: $val)
                 .keyboardType(.numbersAndPunctuation)
                 .multilineTextAlignment(.trailing)
-                .onChange(of: text) { newValue in
+                .onChange(of: val) { newValue in
                     item.value = formatter.number(from: newValue)?.intValue ?? 0
                 }
-        }.task { text = "\(item.value)" }
+        }.task {
+            if(item.value == 0) {
+                val = ""
+            } else {
+                val = "\(item.value)"
+            }
+        }
     }
 }
 
@@ -131,12 +160,13 @@ struct SumCalculationStepContentViewPreviews: PreviewProvider {
     static var previews: some View {
         SumCalculationStepContentView(
             items: [
-                SumCalculationItem(id: "1", label: "Removal Fees"),
-                SumCalculationItem(id: "2", label: "Conveyancing")
+                SumCalculationItem(id: "1", text: "Removal Fees"),
+                SumCalculationItem(id: "2", text: "Conveyancing")
             ]
         ).environmentObject(SumCalculationStep(
             identifier: "",
-            type: "Lorum ipsum",
+            type: "currency",
+            text: "Lorum ipsum",
             items: [],
             allowUserToAddItems: false,
             session: Session.buildEmptySession(),
